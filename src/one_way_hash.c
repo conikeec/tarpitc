182603ce1f8a967fa49a4cc82cdd00ff31fd2e10
Suchakra Sharma <suchakra@shiftleft.io>
2019-02-06 10:05:59 -0800

    Add salt and UAF bug

--- src/one_way_hash.c
+++ src/one_way_hash.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <openssl/md5.h>
 
+#define SALT_LEN 8
+
 unsigned char digest[MD5_DIGEST_LENGTH];
 
-void print_hash(unsigned char* md) {
+void log_debug(unsigned char* md) {
     int i;
     printf("MD5: \t\t\t");
     for(i=0; i < MD5_DIGEST_LENGTH; i++) {
@@ -14,19 +16,52 @@ void print_hash(unsigned char* md) {
     printf("\n");
 }
 
+void hash_password(char* pass, char* salt) {
+    char* salted_pass = strcat(pass, salt);
+    MD5((unsigned char*) salted_pass, strlen(salted_pass), digest);
+}
+
+void rot(char *str) {
+  int i =0;
+  for(i=0;str && str[i]; ++i) {
+      if(str[i] >= 'a' && (str[i]+13) <='z') {
+          str[i] = str[i]+13;
+      }
+  }
+  free(str);
+}
+
+char* get_new_salt(char* pass) {
+    char* slt = malloc(SALT_LEN);
+    memset(slt, '\0', SALT_LEN);
+    strncpy(slt, pass, SALT_LEN);
+    return slt;
+}
+
+char* enhance_salt(char* pass) {
+    char* slt = malloc(SALT_LEN);
+    memset(slt, '\0', SALT_LEN);
+    strncpy(slt, pass, SALT_LEN);
+    rot(slt);
+    return slt;
+}
+
 int main(int argc, char *argv[]) {
 
-    if(argc != 2) {
-        printf("ERROR: Input password string\n");
+    if(argc != 3) {
+        printf("ERROR: Input password string and salt\n");
         exit(1);
     }
 
     printf("Password String:\t%s\n", argv[1]);
 
     char* pass = argv[1];
-    MD5((unsigned char*) pass, strlen(pass), digest);
+    char* slt = get_new_salt(pass);
+    hash_password(pass, slt);
+    log_debug(digest);
 
-    print_hash(digest);
+    char* new_slt = enhance_salt(slt);
+    log_debug(slt);
 
     return 0;
 }
